0bed14b64bd3b6559becc6850e7dd6de2b693aed
ploper12zz-beep <ploper12zz@gmail.com>
2026-09-25 13:39:15 +0700

    displaying loading text V2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int totalstep = 29;
+
+int i = 1;
+while (i <= totalstep)
+{
+    Console.WriteLine($"Loading.. file {i} of {totalstep} completed.");
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Checkpoint reached! Saving progress...\n");
+    }
+    i++;
+}
